fe6e73bf2daff152f6efc967686c8025ff67969c
Rajnishr31 <kumarrauson426@gmail.com>
2022-12-15 21:47:52 +0530

    add these changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://RajnishCalifornium:Nish54321@rajnishcalifornium.qhqnlpb.mongodb.net/Rajnish54321-DB",{
+    useNewUrlParser:true
+})
+.then(  ()  => console.log("MongoDb is connected"))
+.catch( err => console.log(err) )
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3002, function() {
