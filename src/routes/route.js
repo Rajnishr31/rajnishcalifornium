1b8e5479043df271c61dc5c384c81698872af545
Rajnishr31 <kumarrauson426@gmail.com>
2022-12-18 23:14:06 +0530

    add these changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
+const BookCollectionController = require("../controllers/bookCollectionController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -15,5 +16,16 @@ router.get("/getUsersData", UserController.getUsersData)
 router.post("/createBook", BookController.createBook  )
 
 router.get("/getBooksData", BookController.getBooksData)
-
+//Q1
+router.post("/BookCollection5", BookCollectionController.BookCollection5  )
+//Q2
+router.get("/BookCollection4", BookCollectionController.BookCollection4  )
+//Q3
+router.post("/BookCollection6", BookCollectionController.BookCollection6  )
+//4
+router.get("/BookCollection7", BookCollectionController.BookCollection7  )
+//Q5
+router.get("/BookCollection8", BookCollectionController.BookCollection8  )
+//Q6
+router.get("/BookCollection9", BookCollectionController.BookCollection9  )
 module.exports = router;
