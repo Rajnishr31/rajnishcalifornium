8d3bedb7635fb5004ebb01c349b226e1b962719a
Rajnishr31 <kumarrauson426@gmail.com>
2022-12-24 21:12:13 +0530

    add

---
+++ src/controllers/userController.js
@@ -0,0 +1,12 @@
+const UserModel= require("../models/userModel")
+
+
+
+
+
+const createUser= async function (req, res) {
+    let body= req.body
+    let userData= await UserModel.create(body)
+    res.send({data: userData})
+}
+module.exports.createUser= createUser
