fe18f061975246e9471b9b0fa4beef120ef4270c
Rajnishr31 <kumarrauson426@gmail.com>
2022-12-29 23:14:20 +0530

    add

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,36 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
+//const userModel = require("../models/userModel");
+let authentication = async function(req,res,next){
+try{
+  let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
 
-    next()
+  
+  if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
+  let decodedToken = jwt.verify(token, "functionup-californium-very-very-secret-key");
+  req.decodedToken = decodedToken
+  if (!decodedToken)
+    return res.status(401).send({ status: false, msg: "token is invalid" })
+    next();
 }
+catch(error){
+  res.status(401).send({msg:error})
+}}
+
 
 
 const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+  try{
+    let userToBeModified = req.params.userId
+    let userLoggedIn = req.decodedToken .userId
+    if(userToBeModified != userLoggedIn) return res.status(400).send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+
     next()
 }
+catch(error){
+  res.status(400).send({msg:error})
+}
+}
+
+module.exports.authentication = authentication;
+module.exports.authorise = authorise;
